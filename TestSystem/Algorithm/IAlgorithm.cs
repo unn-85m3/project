8a70816cc5004f6b4e25bcb9fe1a115d0451afff
ily294 <ily294@ya.ru>
2014-09-27 15:21:02 +0400

    Залил весь проект

---
+++ TestSystem/Algorithm/IAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSystem.Algorithm
+{
+    /**
+     * Этот интерфейс должны наследовать все алгоритмы
+     */
+    interface IAlgorithm
+    {
+    }
+}
