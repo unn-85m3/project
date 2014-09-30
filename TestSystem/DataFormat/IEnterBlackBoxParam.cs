cb52a8d48d6466c86e4c7fb5799ba609345bdcca
ily294 <ily294@ya.ru>
2014-09-30 10:02:16 +0400

    add function to IEnterBlackBoxParam

--- TestSystem/DataFormat/IEnterBlackBoxParam.cs
+++ TestSystem/DataFormat/IEnterBlackBoxParam.cs
@@ -11,5 +11,13 @@ namespace TestSystem.DataFormat
      */
     interface IEnterBlackBoxParam
     {
+        Double p_in_min { get; }
+        Double p_in_max { get; }
+
+        Double p_out_min { get; }
+        Double p_out_max { get; }
+
+        Double comprimate_min { get; }
+        Double comprimate_max { get; } 
     }
 }
