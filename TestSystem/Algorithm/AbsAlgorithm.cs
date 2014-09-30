ace58ad0356eef85fd09c3b4534c4261c7e00ae6
ily294 <ily294@ya.ru>
2014-09-30 16:38:28 +0400

    add absAlg

--- TestSystem/Algorithm/AbsAlgorithm.cs
+++ TestSystem/Algorithm/AbsAlgorithm.cs
@@ -4,17 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TestSystem.DataFormat;
+using TestSystem.BackBoxFunction;
 
 namespace TestSystem.Algorithm
 {
-
+    /// <summary>
+    /// От него наследуются все алгоритмы.
+    /// </summary>
     abstract class AbsAlgorithm:IAlgorithm
     {
-        protected IEnterBlackBoxParam parameter;
-        protected String name;
-        protected AbsAlgorithm(IEnterBlackBoxParam parameter=null)
+        protected IEnterBlackBoxParam parameter; ///парамтры, в рамках которых проводится оптимизация
+        protected String name="Имя";/// имя алгоритма+имя автора
+        protected IFunction function;///функция для оптимизации
+                                     
+                                     
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        /// <param name="parameter">входные параметры ф-и Ч.Я.</param>
+        protected AbsAlgorithm(IEnterBlackBoxParam parameter,IFunction function)
         {
             this.parameter = parameter;
+            this.function = function;
+            
         }
 
         public string Name
@@ -22,14 +34,22 @@ namespace TestSystem.Algorithm
             get { return name; }
         }
 
+        /// <summary>
+        /// через эту ф-ю можно изменить входные данные алгоритма
+        /// </summary>
+        public IEnterBlackBoxParam enterParam
+        {
+            set { parameter = value; }
+        }
 
+        /// <summary>
+        /// Здесь находится сам алгоритм отптимизации ф-и
+        /// </summary>
+        /// <returns>результат работы алгоритма</returns>
         public abstract IOutBlackBoxParam Calculate();
 
 
 
-        public IEnterBlackBoxParam enterParam
-        {
-            set { parameter=value; }
-        }
+        
     }
 }
