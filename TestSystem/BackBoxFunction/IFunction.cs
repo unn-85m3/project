eca50ede61edc7586f36cb1016bb13b43eb5343c
ily294 <ily294@ya.ru>
2014-09-30 14:47:16 +0400

    add BlackBoxFunction

---
+++ TestSystem/BackBoxFunction/IFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSystem.DataFormat;
+
+namespace TestSystem.BackBoxFunction
+{
+    interface IFunction
+    {
+        IOutBlackBoxParam Calculate(Double x1,Double x2);
+    }
+}
