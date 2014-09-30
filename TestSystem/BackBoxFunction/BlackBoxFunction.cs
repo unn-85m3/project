5ebf013635b71e9a6c7d9ef52b67ed8744aa4377
ily294 <ily294@ya.ru>
2014-09-30 15:05:21 +0400

    Stryct Changes

---
+++ TestSystem/BackBoxFunction/BlackBoxFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSystem.DataFormat;
+
+namespace TestSystem.BackBoxFunction
+{
+    class BlackBoxFunction : IFunction, IOutBlackBoxParam
+    {
+        private Double cost;
+        public IOutBlackBoxParam Calculate(double x1, double x2)
+        {
+            cost = Math.Sin(x1) * Math.Cos(x2);
+            return this;
+        }
+
+        public double Cost
+        {
+            get { return cost; }
+        }
+    }
+}
