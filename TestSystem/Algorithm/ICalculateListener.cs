b34b27a59c87170c48d8d53bd4e13d740ad7bada
ily294 <ily294@ya.ru>
2014-09-30 10:17:49 +0400

    add listener intrface

---
+++ TestSystem/Algorithm/ICalculateListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSystem.DataFormat;
+
+namespace TestSystem.Algorithm
+{
+    interface ICalculateListener
+    {
+        void onCalculate(IOutBlackBoxParam param);
+    }
+}
