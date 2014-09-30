f1cd20e2edd506034ec2c7695024c0541f2ed4b3
ily294 <ily294@ya.ru>
2014-09-30 09:26:22 +0400

    add methods to interface

--- TestSystem_beta/Algorithm/IAlgorithm.cs
+++ TestSystem_beta/Algorithm/IAlgorithm.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestSystem.DataFormat;
 
 namespace TestSystem.Algorithm
 {
-    /**
-     * Этот интерфейс должны наследовать все алгоритмы
-     */
+   
     interface IAlgorithm
     {
+        String Name { get; }///name of algorithm
+        IOutBlackBoxParam Calculate();///calculate algorithm
     }
 }
