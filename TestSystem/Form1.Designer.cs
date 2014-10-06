d32e10a202a680d85380f4554ea34a5e45101231
Agach <Ilya_agachkin@mail.ru>
2014-10-06 22:27:15 +0400

    1

--- TestSystem/Form1.Designer.cs
+++ TestSystem/Form1.Designer.cs
@@ -44,76 +44,88 @@
         /// <summary>
         /// Создание вкладок и таблиц на них.
         /// </summary>
-        //private void InitTab()
-        //{
-        //    this.tabControl1 = new System.Windows.Forms.TabControl();
-        //    this.tabPages = new System.Windows.Forms.TabPage[Algorithms.Count];
-        //    this.dataGridViews = new System.Windows.Forms.DataGridView[Algorithms.Count];
-        //    this.tabControl1.SuspendLayout();
+        private void InitTab()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPages = new System.Windows.Forms.TabPage[Algorithms.Length];
+            this.dataGridViews = new System.Windows.Forms.DataGridView[Algorithms.Length];
+            this.tabControl1.SuspendLayout();
 
-        //    for (int i = 0; i < Algorithms.Count; i++)
-        //    {
-        //        tabPages[i] = new System.Windows.Forms.TabPage();
-        //        dataGridViews[i] = new System.Windows.Forms.DataGridView();
+            for (int i = 0; i < Algorithms.Length; i++)
+            {
+                tabPages[i] = new System.Windows.Forms.TabPage();
+                dataGridViews[i] = new System.Windows.Forms.DataGridView();
 
-        //        ((System.ComponentModel.ISupportInitialize)(this.dataGridViews[i])).BeginInit();
+                ((System.ComponentModel.ISupportInitialize)(this.dataGridViews[i])).BeginInit();
 
-        //        this.tabPages[i].SuspendLayout();
+                this.tabPages[i].SuspendLayout();
 
 
-        //        this.tabControl1.Controls.Add(this.tabPages[i]);
-        //    }
-        //    this.SuspendLayout();
-        //    // 
-        //    // tabControl1
-        //    // 
-        //    this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-        //    | System.Windows.Forms.AnchorStyles.Left)
-        //    | System.Windows.Forms.AnchorStyles.Right)));
-        //    this.tabControl1.Location = new System.Drawing.Point(1, 1);
-        //    this.tabControl1.Name = "tabControl1";
-        //    this.tabControl1.SelectedIndex = 0;
-        //    this.tabControl1.Size = new System.Drawing.Size(this.Size.Width - 17, this.Size.Height - 41);
-        //    this.tabControl1.TabIndex = 0;
-        //    // 
-        //    // tabPages
-        //    // 
-        //    for (int i = 0; i < Algorithms.Count; i++)
-        //    {
-        //        this.tabPages[i].Controls.Add(this.dataGridViews[i]);
-        //        this.tabPages[i].Location = new System.Drawing.Point(1, 1);
-        //        this.tabPages[i].Name = "tabPage1";
-        //        this.tabPages[i].Padding = new System.Windows.Forms.Padding(3);
-        //        this.tabPages[i].Size = new System.Drawing.Size(this.tabControl1.Size.Width, this.tabControl1.Size.Height);
-        //        this.tabPages[i].TabIndex = 0;
-        //        this.tabPages[i].Text = Algorithms[i].Name;
-        //        this.tabPages[i].UseVisualStyleBackColor = true;
-        //    }
-        //    // 
-        //    // dataGridViews
-        //    // 
-        //    for (int i = 0; i < Algorithms.Count; i++)
-        //    {
-        //        this.dataGridViews[i].Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-        //        | System.Windows.Forms.AnchorStyles.Left)
-        //        | System.Windows.Forms.AnchorStyles.Right)));
-        //        this.dataGridViews[i].ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        //        this.dataGridViews[i].Location = new System.Drawing.Point(1, 1);
-        //        this.dataGridViews[i].Name = "dataGridView1";
-        //        this.dataGridViews[i].Size = new System.Drawing.Size(this.tabPages[i].Size.Width - 1, this.tabPages[i].Size.Height - 1);
-        //        this.dataGridViews[i].TabIndex = 0;
-        //    }
-            
-        //    this.tabControl1.ResumeLayout(false);
-        //    for (int i = 0; i < Algorithms.Count; i++)
-        //    {
-        //        this.tabPages[i].ResumeLayout(false);
-        //        ((System.ComponentModel.ISupportInitialize)(this.dataGridViews[i])).EndInit();
-        //    }
+                this.tabControl1.Controls.Add(this.tabPages[i]);
+            }
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(this.Size.Width - 17, this.Size.Height - 41);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPages
+            // 
+            for (int i = 0; i < Algorithms.Length; i++)
+            {
+                this.tabPages[i].Controls.Add(this.dataGridViews[i]);
+                this.tabPages[i].Location = new System.Drawing.Point(1, 1);
+                this.tabPages[i].Name = "tabPage1";
+                this.tabPages[i].Padding = new System.Windows.Forms.Padding(3);
+                this.tabPages[i].Size = new System.Drawing.Size(this.tabControl1.Size.Width, this.tabControl1.Size.Height);
+                this.tabPages[i].TabIndex = 0;
+                this.tabPages[i].Text = Algs[i].Name;
+                this.tabPages[i].UseVisualStyleBackColor = true;
+            }
+            // 
+            // dataGridViews
+            // 
+            for (int i = 0; i < Algorithms.Length; i++)
+            {
+                this.dataGridViews[i].Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                | System.Windows.Forms.AnchorStyles.Left)
+                | System.Windows.Forms.AnchorStyles.Right)));
+                this.dataGridViews[i].ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+                this.dataGridViews[i].Location = new System.Drawing.Point(1, 1);
+                this.dataGridViews[i].Name = "dataGridView1";
+                this.dataGridViews[i].Size = new System.Drawing.Size(this.tabPages[i].Size.Width - 1, this.tabPages[i].Size.Height - 1);
+                this.dataGridViews[i].TabIndex = 0;
+            }
 
-        //    this.Controls.Add(this.tabControl1);
-        //    this.ResumeLayout(false);
-        //}
+            for (int i = 0; i < Algorithms.Length; i++)
+            {
+                dataGridViews[i].Columns.Add("Task", "Задача");
+                dataGridViews[i].Columns.Add("Time", "t, мин.");
+                dataGridViews[i].Columns.Add("Func", "Кол-во вызовов ф-ции");
+                dataGridViews[i].Columns.Add("BB", "Кол-во вызовов ЧЯ");
+                dataGridViews[i].Columns.Add("Cost", "Стоимость");
+            }
+
+
+            this.tabControl1.ResumeLayout(false);
+            for (int i = 0; i < Algorithms.Length; i++)
+            {
+                this.tabPages[i].ResumeLayout(false);
+                ((System.ComponentModel.ISupportInitialize)(this.dataGridViews[i])).EndInit();
+            }
+
+            this.Controls.Add(this.tabControl1);
+            this.ResumeLayout(false);
+
+            Create_Rows();
+        }
 
         #endregion
 
--- TestSystem/Form1.cs
+++ TestSystem/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TestSystem.test_system;
 using TestSystem.BlackBox;
+using TestSystem.Algorithm;
 
 namespace TestSystem
 {
@@ -16,79 +17,87 @@ namespace TestSystem
     {
         private TestSystem.test_system.TestSystem Algorithms;
         private List<Tasks.ITaskPackage> Tasks;
+        private List<IAlgorithm> Algs;
 
 
         public Form1()
         {
             InitializeComponent();
-            Create_Algorithms();
+            Create_TestSystem();
         }
 
         /// <summary>
         /// Заглушка на алгоритмы.
         /// </summary>
-        private void Create_Algorithms()
+        private void Create_TestSystem()
         {
+            Algs = new List<IAlgorithm>();
+            Algs.Add(new Algorithm.Benchmark_Algorithm(null, new BlackBoxFunction()));
             Tasks = new List<TestSystem.Tasks.ITaskPackage>();
             DataFormat.DataFormat dtf = new DataFormat.DataFormat();
             dtf.OpenFile("/Tests/test_1.txt");
             Tasks.Add(dtf.GetData());
             Algorithms = new test_system.TestSystem(Tasks, new BlackBoxFunction());
             Algorithms.SetListener(this);
-            //Algorithms.AddAlgorithm(new Algorithm.Benchmark_Algorithm(null, 
+            Algorithms.AddAlgorithm(Algs[0]);
+            InitTab();
+            Algorithms.Test();
             //Algorithms.Add(new Algorithm.Benchmark_Algorithm(null,null));
             //Algorithms.Add(new Algorithm.Genetic_Algorithm(null, null));
             //Create_Tasks();
-            //InitTab();
-            //Init_Table();
+            
+            
         }
 
         /// <summary>
         /// Заглушка на задания
         /// </summary>
-        //private void Create_Tasks()
-        //{
-        //    Tasks = new List<Tasks.Tasks_Base>();
-        //    Tasks.Add(new Tasks.Tasks_Base("Задача 1"));
-        //    Tasks.Add(new Tasks.Tasks_Base("Задача 2"));
-        //    Tasks.Add(new Tasks.Tasks_Base("Задача 3"));
-        //    Tasks.Add(new Tasks.Tasks_Base("Задача 4"));
-        //}
+        private void Create_Rows()
+        {
+            for (int i = 0; i < Algorithms.Length; i++)
+            {
+
+                for (int j = 0; j < Tasks.Count; j++)
+                {
+                    dataGridViews[i].Rows.Add();
+                    dataGridViews[i].Rows[j].Cells[0].Value = Tasks[j].Name;
+                    dataGridViews[i].Rows[j].Cells[1].Value = "";
+                    dataGridViews[i].Rows[j].Cells[2].Value = "";
+                    dataGridViews[i].Rows[j].Cells[3].Value = "";
+                    dataGridViews[i].Rows[j].Cells[4].Value = "";
+                }
+            }
+        }
 
         /// <summary>
         /// Заполнение таблиц.
         /// </summary>
-        //private void Init_Table()
-        //{
-        //    for (int i = 0; i < Algorithms.Count; i++)
-        //    {
-        //        dataGridViews[i].Columns.Add("Task", "Задача");
-        //        dataGridViews[i].Columns.Add("Time", "t, мин.");
-        //        dataGridViews[i].Columns.Add("Func", "Кол-во вызовов ф-ции");
-        //        dataGridViews[i].Columns.Add("BB", "Кол-во вызовов ЧЯ");
-        //        dataGridViews[i].Columns.Add("Cost", "Стоимость");
-        //        for (int j = 0; j < Tasks.Count; j++)
-        //        {
-        //            dataGridViews[i].Rows.Add();
-        //            dataGridViews[i].Rows[j].Cells[0].Value = Tasks[j].Name;
-        //            //dataGridViews[i].Rows[j].Cells[1].Value = "";
-        //            //dataGridViews[i].Rows[j].Cells[2].Value = "";
-        //            //dataGridViews[i].Rows[j].Cells[3].Value = "";
-        //            //dataGridViews[i].Rows[j].Cells[4].Value = "";
-        //        }
-        //    }
-        //}
+        private void Init_Table()
+        {
+            for (int i = 0; i < Algorithms.Length; i++)
+            {
+                
+                for (int j = 0; j < Tasks.Count; j++)
+                {
+                    dataGridViews[i].Rows[j].Cells[0].Value = Tasks[j].Name;
+                    //dataGridViews[i].Rows[j].Cells[1].Value = "";
+                    //dataGridViews[i].Rows[j].Cells[2].Value = "";
+                    //dataGridViews[i].Rows[j].Cells[3].Value = "";
+                    //dataGridViews[i].Rows[j].Cells[4].Value = "";
+                }
+            }
+        }
 
 
 
         public void OnEndCalculate(Algorithm.IAlgorithm alg, Tasks.ITaskPackage task, DataFormat.IOutBlackBoxParam rez, int time)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void OnEndTask(Algorithm.IAlgorithm alg, Tasks.ITaskPackage task, DataFormat.IOutBlackBoxParam rez, int time)
         {
-            throw new NotImplementedException();
+            Init_Table();
         }
     }
 }
