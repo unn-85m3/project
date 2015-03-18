fae864836bb18624dc5c8298316b884b599d5535
ily294 <ily294@ya.ru>
2015-03-18 16:48:21 +0300

    обновил архитектуру, добавил абстрактный график

---
+++ TestSystem/Plot/AbstractPlot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSystem.Plot
+{
+    abstract class AbstractPlot:IPlot
+    {
+        protected List<IPoint> points;
+        public AbstractPlot(List<IPoint> points)
+        {
+            this.points = points;
+        }
+        
+    }
+}
---
+++ TestSystem/Plot/IPlot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSystem.Plot
+{
+    interface IPlot
+    {
+        void AddPoint(IPoint point);
+    }
+}
