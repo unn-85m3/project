f854165f5935816f80d1ee602454cd60db7926b1
ily294 <ily294@ya.ru>
2015-03-18 21:38:51 +0300

    добавил сэйвер

---
+++ TestSystem/Saver/ISaver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saver
+{
+    interface ISaver
+    {
+
+        int Save(String name,String value);
+        String Get(String name,String defaullValue);
+        int Delete(String name);
+    }
+}
