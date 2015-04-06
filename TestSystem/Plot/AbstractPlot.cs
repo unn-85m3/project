28878ef36465b0799ce613c14fe497f527606d90
Agach <Ilya_agachkin@mail.ru>
2015-04-06 17:57:37 +0400

    Draw

--- TestSystem/Plot/AbstractPlot.cs
+++ TestSystem/Plot/AbstractPlot.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestSystem.Drawer;
 
 namespace TestSystem.Plot
 {
-    abstract class AbstractPlot:IPlot
+    abstract class AbstractPlot:Form_Draw, IPlot
     {
         protected List<IPoint> points;
         public AbstractPlot(List<IPoint> points)
