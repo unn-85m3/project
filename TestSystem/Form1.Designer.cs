edfc26c5ef5438226b401f5e41191684b07f9ef6
Agach <Ilya_agachkin@mail.ru>
2015-04-17 08:33:02 +0400

    Батон

--- TestSystem/Form1.Designer.cs
+++ TestSystem/Form1.Designer.cs
@@ -30,13 +30,25 @@
         {
             this.components = new System.ComponentModel.Container();
             this.toolTipTabPages = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(820, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save in Exel";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(907, 338);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Test system";
             this.ResumeLayout(false);
@@ -78,7 +90,7 @@
             this.tabControl1.Location = new System.Drawing.Point(1, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(this.Size.Width - 17, this.Size.Height - 41);
+            this.tabControl1.Size = new System.Drawing.Size(this.Size.Width - 107, this.Size.Height - 41);
             //this.toolTipTabPages.SetToolTip(this.tabControl1, "Результаты работы алгоритмов");
             this.tabControl1.ShowToolTips = true;
             this.tabControl1.TabIndex = 0;
@@ -178,6 +190,7 @@
         private System.Windows.Forms.TabPage[] tabPages;
         private System.Windows.Forms.DataGridView[] dataGridViews;
         private System.Windows.Forms.ToolTip toolTipTabPages;
+        private System.Windows.Forms.Button button1;
     }
 }
 
